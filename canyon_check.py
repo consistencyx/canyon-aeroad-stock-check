e6763c294e4a1c25bc663fb1279b1bbb91a2c559
consistencyx <bastian.wehking@googlemail.com>
2026-03-12 01:20:28 +0100

    Create canyon_check.py

---
+++ canyon_check.py
@@ -0,0 +1,30 @@
+import requests
+
+PRODUCT_ID = "4034"
+COLOR = "R107_P01"
+SIZE = "L"
+
+BOT_TOKEN = "DEIN_TELEGRAM_TOKEN"
+CHAT_ID = "402782250"
+
+url = f"https://www.canyon.com/on/demandware.store/Sites-RoW-Site/en/Product-Variation?pid={PRODUCT_ID}"
+
+r = requests.get(url)
+data = r.json()
+
+size_available = False
+
+for attr in data["variationAttributes"]:
+    if attr["id"] == "frameSize":
+        for v in attr["values"]:
+            if v["displayValue"] == SIZE and v["selectable"]:
+                size_available = True
+
+if size_available:
+    requests.get(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={
+            "chat_id": CHAT_ID,
+            "text": "Aeroad CF SLX 7 Di2 weiß Größe L verfügbar"
+        }
+    )
