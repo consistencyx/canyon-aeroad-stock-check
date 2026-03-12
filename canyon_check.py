672dba166b81dc3439fe6a0f34841f4af079c0e0
consistencyx <bastian.wehking@googlemail.com>
2026-03-12 01:34:00 +0100

    Update canyon_check.py

--- canyon_check.py
+++ canyon_check.py
@@ -1,7 +1,6 @@
 import requests
 
 PRODUCT_ID = "4034"
-COLOR = "R107_P01"
 SIZE = "L"
 
 BOT_TOKEN = "8703701255:AAHP0yakwBDuOEtU-LzzxUASkY_nHX194QY"
@@ -10,28 +9,28 @@ CHAT_ID = "402782250"
 url = f"https://www.canyon.com/on/demandware.store/Sites-RoW-Site/en/Product-Variation?pid={PRODUCT_ID}"
 
 r = requests.get(url)
-data = r.json()
 
-size_available = False
+try:
+    data = r.json()
+except:
+    print("API Antwort nicht lesbar")
+    exit()
 
-for attr in data["variationAttributes"]:
-    if attr["id"] == "frameSize":
-        for v in attr["values"]:
-            if v["displayValue"] == SIZE and v["selectable"]:
-                size_available = True
+attrs = data.get("variationAttributes")
 
-if size_available:
-    requests.get(
-        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-        params={
-            "chat_id": CHAT_ID,
-            "text": "Aeroad CF SLX 7 Di2 weiß Größe L verfügbar"
-        }
-    )
-requests.get(
-    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    params={
-        "chat_id": CHAT_ID,
-        "text": "Testnachricht GitHub Script läuft"
-    }
-)
+if not attrs:
+    print("Keine Variationsdaten gefunden")
+    exit()
+
+for attr in attrs:
+    if attr.get("id") == "frameSize":
+        for v in attr.get("values", []):
+            if v.get("displayValue") == SIZE and v.get("selectable"):
+                requests.get(
+                    f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+                    params={
+                        "chat_id": CHAT_ID,
+                        "text": "Aeroad CF SLX 7 Di2 Weiß Größe L verfügbar!"
+                    }
+                )
+                print("Bike verfügbar")
