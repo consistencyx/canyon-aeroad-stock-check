c6bd3e6f497e91d608b34265ba39580029f68860
consistencyx <bastian.wehking@googlemail.com>
2026-03-12 01:43:14 +0100

    Update canyon_check.py

--- canyon_check.py
+++ canyon_check.py
@@ -2,6 +2,7 @@ import requests
 
 PRODUCT_ID = "4034"
 SIZE = "L"
+COLOR = "R107_P01"
 
 BOT_TOKEN = "8703701255:AAHP0yakwBDuOEtU-LzzxUASkY_nHX194QY"
 CHAT_ID = "402782250"
@@ -16,19 +17,31 @@ try:
 
     attrs = data.get("variationAttributes", [])
 
+    size_ok = False
+    color_ok = False
+
     for attr in attrs:
+
         if attr.get("id") == "frameSize":
             for v in attr.get("values", []):
                 if v.get("displayValue") == SIZE and v.get("selectable"):
-                    available = True
+                    size_ok = True
+
+        if attr.get("id") == "color":
+            for v in attr.get("values", []):
+                if v.get("value") == COLOR and v.get("selectable"):
+                    color_ok = True
+
+    if size_ok and color_ok:
+        available = True
 
 except Exception as e:
     message = f"Canyon Check Fehler: {e}"
 
 if available:
-    message = "Aeroad CF SLX 7 Di2 Größe L: VERFÜGBAR"
+    message = "Aeroad CF SLX 7 Di2 Weiß Größe L: VERFÜGBAR"
 else:
-    message = "Aeroad CF SLX 7 Di2 Größe L: NICHT verfügbar"
+    message = "Aeroad CF SLX 7 Di2 Weiß Größe L: NICHT verfügbar"
 
 requests.get(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
